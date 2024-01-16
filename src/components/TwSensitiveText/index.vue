68fc87cc82f4abca25dc767d42f28be261b11af1
Cooper <hi.CooperZhu@gmail.com>
2024-01-16 14:03:40 +0800

    优化 TwSensitiveText

--- src/components/TwSensitiveText/index.vue
+++ src/components/TwSensitiveText/index.vue
@@ -2,30 +2,14 @@
   <span class="tw-sensitive-text">
     <slot v-if="displayValue.length > 0" name="prefix" />
     <span>{{ displayValue }}</span>
-    <SvgIcon v-if="!props.disabled && displayValue.length > 0" :name="show ? 'basic-eye_open' : 'basic-eye_close'" @click="toggleEnable" />
+    <SvgIcon v-if="!props.disabled && displayValue.length > 0" :name="showRaw ? 'basic-eye_open' : 'basic-eye_close'" @click="toggleEnable" />
   </span>
 </template>
-
-<script lang="ts">
-enum DisplayMode {
-  fixLength = 'fix-length',
-  followContent = 'follow-content',
-}
-
-enum DesensitiveType {
-  all = 'all',
-  part = 'part',
-}
-
-enum DesensitiveMode {
-  startEnd = 'start-end',
-  middle = 'middle',
-}
-</script>
 
 <script setup lang="ts">
 import { onMounted, ref, watch } from 'vue';
 import SvgIcon from '@/components/SvgIcon/index.vue';
+import { DisplayMode, DesensitiveType, DesensitiveMode } from './types';
 
 const props = defineProps({
   /**
@@ -72,7 +56,7 @@ const props = defineProps({
    * 当displayMode="fix-length"，需要设置
    */
   displayLength: {
-    type: Number,
+    type: [String, Number],
     required: false,
     default: 10,
   },
@@ -121,7 +105,7 @@ const props = defineProps({
    * 默认值："2"
    */
   start: {
-    type: Number,
+    type: [String, Number],
     required: false,
     default: 2,
   },
@@ -132,7 +116,7 @@ const props = defineProps({
    * 默认值："2"
    */
   end: {
-    type: Number,
+    type: [String, Number],
     required: false,
     default: 2,
   },
@@ -145,12 +129,12 @@ const props = defineProps({
 });
 
 const displayValue = ref('');
-const show = ref(props.disabled);
+const showRaw = ref(props.disabled);
 
 watch(
   () => [props.raw, props.replace, props.displayMode, props.displayLength, props.type, props.mode, props.start, props.end, props.disabled],
   () => {
-    show.value = props.disabled;
+    showRaw.value = props.disabled;
     onInput();
   },
 );
@@ -160,7 +144,7 @@ onMounted(() => {
 });
 
 function toggleEnable() {
-  show.value = !show.value;
+  showRaw.value = !showRaw.value;
   onInput();
 }
 
@@ -171,7 +155,7 @@ function onInput() {
     return;
   }
 
-  if (props.disabled || show.value) {
+  if (props.disabled || showRaw.value) {
     displayValue.value = props.raw;
     return;
   }
@@ -209,57 +193,61 @@ function fixContent(): string {
 }
 
 function fixLength_all(): string {
-  return props.replace.repeat(props.displayLength);
+  return props.replace.repeat(Number(props.displayLength));
 }
 
 function fixLength_part(): string {
+  const propsDisplayLength = Number(props.displayLength);
+  const propsStart = Number(props.start);
+  const propsEnd = Number(props.end);
+
   switch (props.mode) {
     case DesensitiveMode.startEnd:
       // 优先start，不足补*
-      if (props.displayLength <= props.raw.length) {
-        if (props.displayLength <= props.start) {
-          return props.replace.repeat(props.displayLength);
+      if (propsDisplayLength <= props.raw.length) {
+        if (propsDisplayLength <= propsStart) {
+          return props.replace.repeat(propsDisplayLength);
         } else {
-          if (props.displayLength <= props.start + props.end) {
-            return props.replace.repeat(props.start).concat(props.raw.substring(props.start, props.displayLength));
+          if (propsDisplayLength <= propsStart + propsEnd) {
+            return props.replace.repeat(propsStart).concat(props.raw.substring(propsStart, propsDisplayLength));
           } else {
             return props.replace
-              .repeat(props.start)
-              .concat(props.raw.substring(props.start, props.displayLength - props.end))
-              .concat(props.replace.repeat(props.end));
+              .repeat(propsStart)
+              .concat(props.raw.substring(propsStart, propsDisplayLength - propsEnd))
+              .concat(props.replace.repeat(propsEnd));
           }
         }
       } else {
-        if (props.start >= props.raw.length) {
-          return props.replace.repeat(props.displayLength);
+        if (propsStart >= props.raw.length) {
+          return props.replace.repeat(propsDisplayLength);
         } else {
           return props.replace
-            .repeat(props.start)
-            .concat(props.raw.substring(props.start))
-            .concat(props.replace.repeat(props.displayLength - props.raw.length));
+            .repeat(propsStart)
+            .concat(props.raw.substring(propsStart))
+            .concat(props.replace.repeat(propsDisplayLength - props.raw.length));
         }
       }
 
     case DesensitiveMode.middle:
       // 优先start，不足补*
-      if (props.displayLength <= props.raw.length) {
-        if (props.displayLength <= props.start) {
-          return props.raw.substring(0, props.displayLength);
+      if (propsDisplayLength <= props.raw.length) {
+        if (propsDisplayLength <= propsStart) {
+          return props.raw.substring(0, propsDisplayLength);
         } else {
-          if (props.displayLength <= props.start + props.end) {
-            return props.raw.substring(0, props.start).concat(props.replace.repeat(props.displayLength - props.start));
+          if (propsDisplayLength <= propsStart + propsEnd) {
+            return props.raw.substring(0, propsStart).concat(props.replace.repeat(propsDisplayLength - propsStart));
           } else {
             return props.raw
-              .substring(0, props.start)
-              .concat(props.replace.repeat(props.displayLength - props.start - props.end))
-              .concat(props.raw.substring(props.displayLength - props.end, props.displayLength));
+              .substring(0, propsStart)
+              .concat(props.replace.repeat(propsDisplayLength - propsStart - propsEnd))
+              .concat(props.raw.substring(propsDisplayLength - propsEnd, propsDisplayLength));
           }
         }
       } else {
-        if (props.start > props.raw.length) {
-          return props.raw.concat(props.replace.repeat(props.displayLength - props.raw.length));
+        if (propsStart > props.raw.length) {
+          return props.raw.concat(props.replace.repeat(propsDisplayLength - props.raw.length));
         } else {
-          return props.raw.substring(0, props.start).concat(props.replace.repeat(props.displayLength - props.start));
+          return props.raw.substring(0, propsStart).concat(props.replace.repeat(propsDisplayLength - propsStart));
         }
       }
   }
@@ -277,24 +265,27 @@ function fixContent_all(): string {
 }
 
 function fixContent_part(): string {
+  const propsStart = Number(props.start);
+  const propsEnd = Number(props.end);
+
   switch (props.mode) {
     case DesensitiveMode.startEnd:
-      if (props.raw.length <= props.start + props.end) {
+      if (props.raw.length <= propsStart + propsEnd) {
         return props.replace.repeat(props.raw.length);
       } else {
         return props.replace
-          .repeat(props.start)
-          .concat(props.raw.substring(props.start, props.raw.length - props.end))
-          .concat(props.replace.repeat(props.end));
+          .repeat(propsStart)
+          .concat(props.raw.substring(propsStart, props.raw.length - propsEnd))
+          .concat(props.replace.repeat(propsEnd));
       }
     case DesensitiveMode.middle:
-      if (props.raw.length <= props.start + props.end) {
+      if (props.raw.length <= propsStart + propsEnd) {
         return props.raw;
       } else {
         return props.raw
-          .substring(0, props.start)
-          .concat(props.replace.repeat(props.raw.length - (props.start + props.end)))
-          .concat(props.raw.substring(props.raw.length - props.end));
+          .substring(0, propsStart)
+          .concat(props.replace.repeat(props.raw.length - (propsStart + propsEnd)))
+          .concat(props.raw.substring(props.raw.length - propsEnd));
       }
   }
 
